d21e261c9bbba1090078d2449963079ac083e679
Ashly Lau <ashlylau@gmail.com>
2020-01-14 20:50:13 +0000

    refactor code, fix small bug in football weather

--- index.js
+++ index.js
@@ -18,62 +18,99 @@ expressApp.listen(port, () => {
 var http = require("http");
 setInterval(function() {
     http.get("http://umbrella-telegram-bot.herokuapp.com");
+    console.log("ping");
 }, 900000); // every 15 minutes (900000)
 
 const LondonId = 2643741
-const bot = new TelegramBot(process.env.token, {polling: true});
-
-const weatherBitEndpoint = () => (
-  `https://api.weatherbit.io/v2.0/forecast/hourly?city_id=${LondonId}&key=${process.env.weatherBitId}`
-)
-
-const randomFactEndpoint = "https://uselessfacts.jsph.pl/random.json?language=en";
-
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-// bot.onText(/\/rain/, (msg) => {
-// this function is called every morning at 9am
-var j = schedule.scheduleJob('0 9 * * *', function(){
-  const endpoint = weatherBitEndpoint();
-  axios.get(endpoint).then((resp) => {
-    const data = resp.data.data;  // this is an array of 120 hourly predictions (starts from the next hour from when you call this)
+const weatherBitEndpoint = 
+  `https://api.weatherbit.io/v2.0/forecast/hourly?city_id=${LondonId}&key=${process.env.weatherBitId}`;
+const randomFactEndpoint = "https://uselessfacts.jsph.pl/random.json?language=en";
 
-    var rain_periods = [];  // list of intervals where it will rain
-    var i;
-    for (i = 0; i < 24; i++) {
-      const hour_data = data[i];
-      var date = new Date(hour_data.timestamp_utc);
-      const time = date.toLocaleTimeString('en-US');
-      if (time.localeCompare('1:00:00 AM') === 0) {  // stop looking at times after 12 midnight
-        break;
-      }
-      console.log(time);
-      const weather = hour_data.weather;
-      const desc = weather.description;
-      if (desc.includes('rain')) {  //TODO: replace this with a weather code check
-        rain_periods.push(date);
-      }
-      console.log(weather);
-    }
-    console.log(rain_periods);
-    if (rain_periods.length > 0) {
-      const rainMessage = formatRainMessage(rain_periods);
-      bot.sendMessage(process.env.ashChatId, 'Bring your umbrella!!!');
-      bot.sendMessage(process.env.ashChatId, rainMessage);
-    } else {
-      bot.sendMessage(process.env.ashChatId, "Nah its not gonna rain lol")
-    }
-  }, error => { console.log(error); })
+const bot = new TelegramBot(process.env.token, {polling: true});
+
+
+// send message everyday at 9am about whether it will rain
+var j = schedule.scheduleJob('0 9 * * *', function(){
+  sendMessage(weatherBitEndpoint, undefined, rainMessage);
 });
 
+bot.onText(/\/rain/, (msg) => {
+  sendMessage(weatherBitEndpoint, msg, rainMessage);
+});
+
+// weather forecast for the next 24 hours
+bot.onText(/\/weather/, (msg) => {
+  sendMessage(weatherBitEndpoint, msg, weatherMessage);
+});
+
+// message on monday 10am with weather forecast during football training at 7-10pm
+var i = schedule.scheduleJob('0 10 * * 1', function(){
+  sendMessage(weatherBitEndpoint, undefined, footballMessage);
+});
+
+bot.onText(/\/football/, (msg) => {
+  sendMessage(weatherBitEndpoint, msg, footballMessage);
+});
+
+// random fact generator
+bot.onText(/\/random/, (msg) => {
+  sendMessage(randomFactEndpoint, msg, randomMessage);
+});
+
+// random default response messages
+bot.on('message', (msg) => {
+  if (msg.text == "hi" || msg.text == "Hi") {
+    bot.sendMessage(msg.chat.id, "lol hi :)");
+  } else if (msg.text == "whats the weather") {
+    bot.sendMessage(msg.chat.id, "use the \\weather command");
+  }
+});
+
+
+function sendMessage(endpoint, msg, generateMessage) {
+  const msgId = msg.chat.id || process.env.ashChatId;
+  axios.get(endpoint).then((resp) => {
+    generateMessage(resp, msgId);
+  }, error => { console.log(error); });
+}
+
+function rainMessage(resp, msgId) {
+  // this is an array of 120 hourly predictions (starts from the next hour from when you call this)
+  const data = resp.data.data;
+
+  var rain_periods = [];  // list of intervals where it will rain
+  for (var i = 0; i < 24; i++) {
+    const hour_data = data[i];
+    var date = new Date(hour_data.timestamp_utc);
+    const time = date.toLocaleTimeString('en-US');
+    if (time.localeCompare('1:00:00 AM') === 0) {  // stop looking at times after 12 midnight
+      break;
+    }
+    console.log(time);
+    const weather = hour_data.weather;
+    const desc = weather.description;
+    if (desc.includes('rain')) {  //TODO: replace this with a weather code check
+      rain_periods.push(date);
+    }
+    console.log(weather);
+  }
+  console.log(rain_periods);
+  if (rain_periods.length > 0) {
+    bot.sendMessage(msgId, 'Bring your umbrella!!! \u2614');
+    bot.sendMessage(msgId, formatRainMessage(rain_periods));
+  } else {
+    bot.sendMessage(msgId, "Nah its not gonna rain lol")
+  }
+}
+
 function formatRainMessage(rain_periods) {
-  rainMessage = "It's gonna rain from";
+  var rainMessage = "It's gonna rain from";
   var start = rain_periods[0].getHours();
-  var startIndex = 0;
   var end = start;
   var timingRanges = [0];
-  var i;
-  for (i = 1; i < rain_periods.length; i++) {
+  for (var i = 1; i < rain_periods.length; i++) {
     var curr = rain_periods[i].getHours();
     if (curr - end > 1) {
       timingRanges.push(i-1);  // push previous index because that is the end range
@@ -86,134 +123,69 @@ function formatRainMessage(rain_periods) {
   }
   timingRanges.push(rain_periods.length-1);
 
-  var i;
-  for (i = 0; i < timingRanges.length/2; i++) {
+  for (var i = 0; i < timingRanges.length/2; i++) {
     rainMessage = rainMessage + " ";
     rainMessage = rainMessage + rain_periods[timingRanges[i]].toLocaleTimeString('en-US');
     rainMessage = rainMessage + " to ";
     rainMessage = rainMessage + rain_periods[timingRanges[i+1]].toLocaleTimeString('en-US');
   }
   rainMessage = rainMessage + " today."
-  console.log(rainMessage);
   return rainMessage;
 }
 
-bot.onText(/\/weather/, (msg) => {
-  const chatId = msg.chat.id;
-  const endpoint = weatherBitEndpoint();
-  axios.get(endpoint).then((resp) => {
-    const data = resp.data.data;  // this is an array of 120 hourly predictions (starts from the next hour from when you call this)
-    console.log(data);
+function weatherMessage(resp, msgId) {
+  // this is an array of 120 hourly predictions (starts from the next hour from when you call this)
+  const data = resp.data.data;
 
-    var rain_periods = [];  // list of intervals where it will rain
-    var weatherInfoList = [];
-    var i;
-    for (i = 0; i < 24; i++) {
-      const hour_data = data[i];
-      var date = new Date(hour_data.timestamp_utc);
-      const time = date.toLocaleTimeString('en-US');
-
-      console.log(time);
-      const weather = hour_data.weather;
-      const desc = weather.description;
-
-      var weatherInfo = [date, weather];
-      weatherInfoList.push(weatherInfo);
-    }
-    bot.sendMessage(chatId, formatWeatherMessage(weatherInfoList));
-  }, error => { console.log(error); })
-});
-
-//TODO: fix this
-function formatWeatherMessage(weatherInfoList) {
-  var message = "Here is the weather forecast:\n";
-  var weatherDesc = weatherInfoList[0][1].description;
-  var startTime = weatherInfoList[0][0];
-  var endTime = startTime;
-  var i;
-  for (i = 1; i < weatherInfoList.length; i++) {
-    const currWeatherDesc = weatherInfoList[i][1].description;
-    const currTime = weatherInfoList[i][0];
-    console.log(currTime);
-    console.log(weatherInfoList[i][1]);
-    if (weatherDesc.localeCompare(currWeatherDesc) == 0) {
-      endTime = currTime;
-    } else {
-      message = message + weatherDesc;
-      message = message + ": "
-      message = message + startTime.toLocaleTimeString('en-US');
-      message = message + " - "
-      message = message + endTime.toLocaleTimeString('en-US');
-      message = message + "\n"
-      weatherDesc = currWeatherDesc;
-      startTime = endTime = currTime;
-    }
+  console.log(data);
+  var weatherForecast = [];
+  for (var i = 0; i < 24; i++) {
+    var date = new Date(data[i].timestamp_utc);
+    const timeStr = date.toLocaleTimeString('en-US');
+    const time = timeStr.split(":");
+    weatherForecast.push((time[0].length == 1 ? "0" : "") + time[0] + time[2].substr(3, 2));
+    weatherForecast.push(": ");
+    weatherForecast.push(data[i].temp);
+    weatherForecast.push(" deg, ");
+    weatherForecast.push(data[i].weather.description);
+    weatherForecast.push("\n");
   }
-  message = message + weatherDesc;
-  message = message + ": "
-  message = message + startTime.toLocaleTimeString('en-US');
-  message = message + " - "
-  message = message + endTime.toLocaleTimeString('en-US');
-  return message;
+  bot.sendMessage(msgId, "Here's the weather for the next 24 hours \u26C5");
+  bot.sendMessage(msgId, weatherForecast.join(""));
 }
 
-// message on monday 10am with weather forecast during football training at 7-10pm
-var i = schedule.scheduleJob('0 10 * * 1', function(){
-  sendFootballWeather();
-});
+function footballMessage(resp, msgId) {
+  // this is an array of 120 hourly predictions (starts from the next hour from when you call this)
+  const data = resp.data.data;
 
-bot.onText(/\/football/, (msg) => {
-  sendFootballWeather();
-});
+  // filter appropriate timings
+  var footballTiming = data.filter(function(day) {
+    var date = new Date(day.timestamp_utc);
+    const time = date.toLocaleTimeString('en-US');
+    return ["7:00:00 PM", "8:00:00 PM", "9:00:00 PM"].includes(time) && date.getDay() == 1;
+  });
 
-function sendFootballWeather() {
-  const endpoint = weatherBitEndpoint();
-  axios.get(endpoint).then((resp) => {
-    // this is an array of 120 hourly predictions (starts from the next hour from when you call this)
-    const data = resp.data.data;
-
-    // filter appropriate timings
-    var footballTiming = data.filter(function(day) {
-      var date = new Date(day.timestamp_utc);
-      const time = date.toLocaleTimeString('en-US');
-      return ["7:00:00 PM", "8:00:00 PM", "9:00:00 PM"].includes(time) && date.getDay() == 1;
-    });
-
+  if (footballTiming.length != 0) {
     var weather = new Set();
     var temp = 0;
     for (timing of footballTiming) {
       weather.add(timing.weather.description);
       temp += timing.temp/3;
     }
-
     // get today's date
     var date = new Date(footballTiming[0].timestamp_utc);
     var dateString = date.getDate() + " " + months[date.getMonth()];
 
-    bot.sendMessage(process.env.ashChatId, "Football training 7-10pm today (" + dateString + ") \u26BD");
-    bot.sendMessage(process.env.ashChatId, 'Weather: ' + Array.from(weather).join(', ') +
-                                           ', Temp: ' + temp.toFixed(2));
-  }, error => { console.log(error); });
-}
-
-// random fact generator
-bot.onText(/\/random/, (msg) => {
-  sendRandomFact();
-});
-
-function sendRandomFact() {
-  axios.get(randomFactEndpoint).then((resp) => {
-    const randomFact = resp.data.text;
-    bot.sendMessage(process.env.ashChatId, "Here's a random ass fact:");
-    bot.sendMessage(process.env.ashChatId, randomFact);
-  }, error => { console.log(error); });
-}
-
-// random default response messages
-bot.on('message', (msg) => {
-  if (msg.text == "hi" || msg.text == "Hi") {
-    bot.sendMessage(msg.chat.id, "lol hi :)");
-  } else if (msg.text == "whats the weather") {
-    bot.sendMessage(msg.chat.id, "use the \\weather command");
+    bot.sendMessage(msgId, "Football training 7-10pm today (" + dateString + ") \u26BD");
+    bot.sendMessage(msgId, 'Weather: ' + Array.from(weather).join(', ') +
+                           ', Temp: ' + temp.toFixed(2));
+  } else {
+    bot.sendMessage(msgId, "Weather data not available yet \uD83D\uDE22");
   }
-});
+}
+
+function randomMessage(resp, msgId) {
+  const randomFact = resp.data.text;
+  bot.sendMessage(msgId, "Here's a random ass fact: \uD83D\uDC7E");
+  bot.sendMessage(msgId, randomFact);
+}
